7426d8f4647151a1c1670d42835cf6247ad867db
Jared Harris <jared.harris1@g.austincc.edu>
2019-05-10 19:04:51 -0500

    working with vertical and horizontal checks and against null values

--- main.js
+++ main.js
@@ -1,35 +1,71 @@
 console.log("hello");
 
 let color = [
-    ['red', 'red', 'black', 'red', 'black', 'red', 'black', 'black'],
-    ['null', 'black', 'black', 'black', 'red', 'red', 'black', 'black'],
-    ['red', 'red', 'black', 'black', 'null', 'black', 'black', 'red'],
-    ['black', 'black', 'red', 'red', 'black', 'black', 'red', 'red'],
-    ['red', 'null', 'red', 'red', 'black', 'black', 'black', 'null'],
-    ['black', 'black', 'black', 'null', 'red', 'red', 'black', 'black'],
-    ['red', 'red', 'black', 'black', 'black', 'null', 'black', 'red'],
-    ['black', 'black', 'red', 'red', 'black', 'black', 'red', 'red']
+    ['red',   'red',   'black', 'red',    null,    null,     null,    null],
+    ['black', 'black',  null,   'black', 'red',   'red',    'black', 'black'],
+    ['red',   'red',   'black', 'black',  null,   'black',   null,   'red'],
+    ['black', 'black', 'red',   'red',   'black', 'black',  'red',   'red'],
+    ['red',    null,   'red',   'red',   'black', 'black',  'black',  null],
+    ['black', 'black', 'black',  null,   'red',   'black',   'black', 'black'],
+    ['red',   'red',    null,   'red',   'black',  'black', 'black', 'red'],
+    ['black', 'red',   'red',    null,   'red',    'black', 'red',   'red']
 ];
 //  console.log(color);
 
 function checker(colorChecker) {
-    for (let i = 0; i < colorChecker.length; i++) {
-        for (let j = 0; j < colorChecker[i].length; j++) {
+    for (let row = 0; row <= 4; row++) {
+        for (let col = 0; col <= 7; col++) {
+            if (!colorChecker[col][row] || !colorChecker[col][row + 1]) continue;
 
-            console.log(colorChecker[i][j])
-            console.log(`current color is: ${colorChecker[i][j]}`);
-            console.log("next color is " + colorChecker[i][j + 1]);
-            console.log(`i = ${i}, j = ${j}`)
+            // console.log(color)
+            console.log("----------------------------------------------------")
+            console.log(`current horizontal color is: ${colorChecker[col][row]}`);
+            console.log("next horizontalcolor is " + colorChecker[col][row + 1]);
+            console.log(`col = ${col}, row = ${row}`)
 
-            if (colorChecker[i][j] === colorChecker[i][j + 1] &&
-                colorChecker[i][j] === colorChecker[i][j + 2] &&
-                colorChecker[i][j] === colorChecker[i][j + 3]) {
+            if (
+                colorChecker[col][row] === colorChecker[col][row + 1] &&
+                colorChecker[col][row] === colorChecker[col][row + 2] &&
+                colorChecker[col][row] === colorChecker[col][row + 3]) {
                 return true;
             }
         }
     }
-    // console.log(colorChecker);
+    for (let row = 0; row <= 7; row++) {
+        for (let col = 0; col <= 4; col++) {
+            if (!colorChecker[col][row] || !colorChecker[col][row + 1]) continue;
+
+            // console.log(color)
+            console.log("----------------------------------------------------")
+            console.log(`current horizontal color is: ${colorChecker[col][row]}`);
+            console.log("next horizontalcolor is " + colorChecker[col][row + 1]);
+            console.log(`col = ${col}, row = ${row}`)
+
+            if (
+                colorChecker[col][row] === colorChecker[col + 1][row] &&
+                colorChecker[col][row] === colorChecker[col + 2][row] &&
+                colorChecker[col][row] === colorChecker[col + 3][row]) {
+                return true;
+            }
+        }
+    }
     return false;
 }
 let results = checker(color);
 console.log(results);
+
+
+
+
+    // colorChecker[col][row] === colorChecker[col + 1][row] &&
+    // colorChecker[col][row] === colorChecker[col + 2][row] &&
+    // colorChecker[col][row] === colorChecker[col + 3][row]) 
+
+    // console.log("----------------------------------------------------")
+    // console.log("current vertical color is " + colorChecker[row][col])
+    // console.log("next vertical color is " + colorChecker[row][col + 1])
+    // console.log(`row = ${row}, col = ${col}`)
+    // console.log("----------------------------------------------------")
+
+    // if (colorChecker[i] !== colorChecker[i+1]){ 
+        //     return false;
