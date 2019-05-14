59444c508afed9f9cd701c62b41048e32a3ce3e9
Jared Harris <jared.harris1@g.austincc.edu>
2019-05-13 22:18:47 -0500

    all tests passing

--- test/main.test.js
+++ test/main.test.js
@@ -6,18 +6,34 @@ describe('Checkers test', () => {
         expect(main.hrzChecker([['red', 'red', 'red', 'red']])).to.be.true;
     });
     it('should check if 2d array has four in a row checking vertically', () => {
-        expect(main.vertChecker([
-            ['black', 
-            'black', 
-            'black', 
-            'black']
-        ])).to.be.true;
+        expect(main.vertChecker(
+            [   
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null]
+            ])).to.be.true;
     });
     it('should check if 2d array has four in a row checking diagonally down', () => {
-        expect(main.diaDown([['black', 'black', 'black', 'black']])).to.be.true;
+        expect(main.diagDown(
+            [   
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', 'red', null, 'black', 'black', null, null],
+                ['red', 'red', null, 'red', 'black', 'black', 'black', null],
+                ['red', 'red', null, null, 'black', 'black', null, 'black']
+            ])).to.be.true;
     });
     it('should check if 2d array has four in a row checking diagonally up', () => {
-        expect(main.diagUp([['red', 'red', 'red', 'red']])).to.be.true;
+        expect(main.diagUp(
+            [   
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null],
+                ['red', 'red', null, 'black', 'black', 'black', null, null],
+                ['red', 'red', 'black', null, 'black', 'black', null, null],
+                ['red', 'red', null, null, 'black', 'black', null, null]
+            ])).to.be.true;
     });
     it('should return null if there is an empty space', () => {
         expect(main.hrzChecker([['red','red','null','null']])).to.be.true;
